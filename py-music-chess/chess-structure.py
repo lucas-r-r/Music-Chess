88c8d9166d40ed463d7a00e7809f3c040a4f8f7f
Lucas Rei Ramos <lj@mundo-r.com>
2018-09-19 13:19:24 +0100

    Added a stub for is_king_capturable()

--- py-music-chess/chess-structure.py
+++ py-music-chess/chess-structure.py
@@ -9,16 +9,27 @@ class Game():
     def move_attempt():
         if is_legal() = True:
             do_move()
+    def is_king_capturable():
+        player = turn.the_other_player()
+        destination = turn.the_king.get_square()
+        for piece in player.list_of_pieces:
+            if piece.move_eval(destination, special = none) == True:
+                return True
+        return False
+
+
 
     def do_move():
         pass
 
 class Piece():
     self.color = color
+    self.list_of_pieces = []
     pass
 
 class Pawn():
     move_eval(self, dest):
         pass
+
 
 #...etc
